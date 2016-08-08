3ed6d8b317be102f034ce1bdd2762e07a3d2c438
dattaprasad <dattaprasad.mundada@pubmatic.com>
2016-08-08 16:24:55 +0530

    Advance Changes

--- js/calender.controller.js
+++ js/calender.controller.js
@@ -33,57 +33,81 @@ function CalenderCntrl($scope) {
             description: "Discussion of Product design & Architectural view of the System",
             day: 20,
             month: 9,
-            year: 2016
+            year: 2016,
+            fromHour: 12,
+            fromMinute: 23,
+            toHour: 16,
+            toMinute: 56
         },
         {
             title: "Lunch With Harvey Spectre ",
             description: "Legal Adivisory",
             day: 28,
             month: 6,
-            year: 2016
+            year: 2016,
+            fromHour: 6,
+            fromMinute: 23,
+            toHour: 10,
+            toMinute: 6
         },
         {
             title: "Dinner With Chandller Bing",
             description: "Statistical analysis and data reconfiguration",
-            day: 20,
+            day: 8,
             month: 6,
-            year: 2016
+            year: 2016,
+            fromHour: 3,
+            fromMinute: 3,
+            toHour: 9,
+            toMinute: 46
         },
         {
             title: "Interview With Clark Kent",
             description: "Daily Planet Interview",
             day: 25,
             month: 4,
-            year: 2016
+            year: 2016,
+            fromHour: 3,
+            fromMinute: 23,
+            toHour: 4,
+            toMinute: 56
         },
         {
             title: "Appointment With Mike Ross",
             description: "Banking Problems Discussion",
-            day: 29,
-            month: 8,
-            year: 2016
+            day: 8,
+            month: 7,
+            year: 2016,
+            fromHour: 3,
+            fromMinute: 23,
+            toHour: 4,
+            toMinute: 56
         }
     ];
-    vm.disableAdd = true;
+    vm.todaysAppointmentList = [];
+    vm.appointmentsForDay = false;
     vm.selectedMonth = 0;
     vm.showEditModal = false;
     vm.selectedYear = 2016;
-    vm.value = 0;
+    vm.day = 0;
+    vm.disableAdd = false;
 
-    vm.showAppointment =  function (item) {
-        console.log(item.value, item.month, item.year);
-        vm.selectedDate = item.value + ' ' + vm.setMonth(item.month) + ' ' + item.year;
+    vm.showAppointment =  function (item, status) {
+        console.log(status);
+        vm.selectedDate = item.day + ' ' + vm.setMonth(item.month) + ' ' + item.year;
         var appointment = vm.checkAppointmentList(item);
-        if(appointment != 0) {
+        vm.disableAdd = status;
+        if(appointment != 0 && status) {
             vm.title = appointment.title;
             vm.description = appointment.description;
-            vm.disableAdd = true;
+            vm.fromTimeHour = appointment.fromHour;
+            vm.fromTimeMinute = appointment.fromMinute;
+            vm.toTimeHour = appointment.toHour;
+            vm.toTimeMinute = appointment.toMinute;
         } else {
-            vm.disableAdd = false;
-            vm.title = '';
-            vm.description = '';
+            vm.clearAppointment();
         }
-        vm.value = item.value;
+        vm.day = item.day;
         vm.showEditModal = true;
     }
 
@@ -93,20 +117,37 @@ function CalenderCntrl($scope) {
             description: vm.description,
             year: vm.selectedYear,
             month: vm.getMonth(vm.selectedMonth),
-            day: vm.value
+            day: vm.day,
+            fromHour: vm.fromTimeHour,
+            fromMinute: vm.fromTimeMinute,
+            toHour: vm.toTimeHour,
+            toMinute: vm.toTimeMinute
         });
         console.log(vm.appointmentList);
         vm.showEditModal = false;
         vm.displayCalendar(vm.getMonth(vm.selectedMonth), vm.selectedYear);
     }
 
+    vm.clearAppointment = function () {
+        vm.title = '';
+        vm.description = '';
+        vm.fromTimeHour = '';
+        vm.fromTimeMinute = '';
+        vm.toTimeHour = '';
+        vm.toTimeMinute = '';
+    }
+
     vm.editAppointment = function () {
         for (var i = 0; i < vm.appointmentList.length; i++) {
-            if(vm.appointmentList[i].day === vm.value
+            if(vm.appointmentList[i].day === vm.day
                 && vm.appointmentList[i].month === vm.getMonth(vm.selectedMonth)
-                && vm.appointmentList[i].year === vm.selectedYear) {
+                && vm.appointmentList[i].year === vm.selectedYear ) {
                         vm.appointmentList[i].title = vm.title;
                         vm.appointmentList[i].description = vm.description;
+                        vm.appointmentList[i].fromHour = vm.fromTimeHour;
+                        vm.appointmentList[i].fromMinute = vm.fromTimeMinute;
+                        vm.appointmentList[i].toHour = vm.toTimeHour;
+                        vm.appointmentList[i].toMinute = vm.toTimeMinute;
                 }
         }
         vm.showEditModal = false;
@@ -114,7 +155,11 @@ function CalenderCntrl($scope) {
 
     vm.deleteAppointment =  function () {
         for (var i = 0; i < vm.appointmentList.length; i++) {
-            if(vm.appointmentList[i].day === vm.value
+            if(vm.appointmentList[i].day === vm.day
+                && vm.appointmentList[i].fromHour === vm.fromTimeHour
+                && vm.appointmentList[i].fromMinute === vm.fromTimeMinute
+                && vm.appointmentList[i].toHour === vm.toTimeHour
+                && vm.appointmentList[i].toMinute === vm.toTimeMinute
                 && vm.appointmentList[i].month === vm.getMonth(vm.selectedMonth)
                 && vm.appointmentList[i].year === vm.selectedYear) {
                         vm.appointmentList.splice(i, 1);
@@ -130,7 +175,21 @@ function CalenderCntrl($scope) {
 
     vm.checkAppointmentList = function (item) {
         for (var i = 0; i < vm.appointmentList.length; i++) {
-            if(vm.appointmentList[i].day === item.value
+            if(vm.appointmentList[i].day === item.day
+                && vm.appointmentList[i].month === item.month
+                && vm.appointmentList[i].fromHour === vm.fromTimeHour
+                && vm.appointmentList[i].fromMinute === vm.fromTimeMinute
+                && vm.appointmentList[i].toHour === vm.toTimeHour
+                && vm.appointmentList[i].toMinute === vm.toTimeMinute
+                && vm.appointmentList[i].year === item.year) {
+                        return vm.appointmentList[i];
+                }
+        }
+        return 0;
+    }
+    vm.checkAppointmentListForDisplay = function (item) {
+        for (var i = 0; i < vm.appointmentList.length; i++) {
+            if(vm.appointmentList[i].day === item.day
                 && vm.appointmentList[i].month === item.month
                 && vm.appointmentList[i].year === item.year) {
                         return vm.appointmentList[i];
@@ -140,6 +199,8 @@ function CalenderCntrl($scope) {
     }
 
     vm.displayCalendar =  function (month, year) {
+        vm.title = '';
+        vm.description = '';
         month = parseInt(month);
         year = parseInt(year);
         vm.list =[];
@@ -149,11 +210,11 @@ function CalenderCntrl($scope) {
         var startingPos = firstOfMonth.getDay();
         days += startingPos;
         for (i = 0; i < startingPos; i++) {
-            vm.list.push({value: 0, month: month, year: year});
+            vm.list.push({day: 0, month: month, year: year});
         }
         for (i = startingPos; i < days; i++) {
-            var appointment = vm.checkAppointmentList({
-                value: (i-startingPos+1),
+            var appointment = vm.checkAppointmentListForDisplay({
+                day: (i-startingPos+1),
                 month: month,
                 year: year
             }),
@@ -161,23 +222,41 @@ function CalenderCntrl($scope) {
             active = (i-startingPos+1) === currentDate.getDate() && month === currentDate.getMonth() && (year-1900) === currentDate.getYear();
             if(appointment != 0) {
                 if(active){
-                    vm.list.push({value: (i-startingPos+1), month: month, year: year, flag: "active", appointment: true});
+                    vm.list.push({day: (i-startingPos+1), month: month, year: year, flag: "active", appointment: true});
                 } else {
-                    vm.list.push({value: (i-startingPos+1), month: month, year: year, appointment: true});
+                    vm.list.push({day: (i-startingPos+1), month: month, year: year, appointment: true});
                 }
             } else {
                 if(active){
-                    vm.list.push({value: (i-startingPos+1), month: month, year: year, flag: "active", appointment: false});
+                    vm.list.push({day: (i-startingPos+1), month: month, year: year, flag: "active", appointment: false});
                 } else {
-                    vm.list.push({value: (i-startingPos+1), month: month, year: year, appointment: false});
+                    vm.list.push({day: (i-startingPos+1), month: month, year: year, appointment: false});
                 }
             }
         }
         for (i=days; i<42; i++)  {
-            vm.list.push({value: 0, month: month, year: year});
+            vm.list.push({day: 0, month: month, year: year});
         }
+
+        vm.appointmentList.forEach(function (appointment) {
+            var date = new Date();
+            if(date.getDate() === appointment.day && date.getMonth() === appointment.month && date.getYear() === (appointment.year-1900)) {
+                    vm.todaysAppointmentList.push(appointment);
+            }
+            console.log(appointment);
+        });
     }
 
+    vm.showTodaysAppointment = function (item) {
+        vm.todaysAppointmentList = [];
+        vm.appointmentsForDay = true;
+        vm.appointmentList.forEach(function (appointment) {
+            if(item.day === appointment.day && item.month === appointment.month && item.year === appointment.year) {
+                    vm.todaysAppointmentList.push(appointment);
+                    console.log(appointment);
+            }
+        });
+    }
 
     vm.getMonth = function (label) {
         for (var i = 0; i < vm.monthList.length; i++) {
