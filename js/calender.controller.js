2ccd9c9bc1ac76eb098f519b0e24a9ef186350c3
dattaprasad <dattaprasad.mundada@pubmatic.com>
2016-08-08 16:43:56 +0530

    Advance Changes

--- js/calender.controller.js
+++ js/calender.controller.js
@@ -345,15 +345,15 @@ function CalenderCntrl($scope) {
         if (month==1 || month==3 || month==5 || month==7 || month==8 || month==10 || month==12)  days=31;
         else if (month==4 || month==6 || month==9 || month==11) days=30;
         else if (month==2)  {
-            if (isLeapYear(year)) { days=29; }
+            if (vm.isLeapYear(year)) { days=29; }
             else { days=28; }
         }
         return (days);
     }
 
     vm.isLeapYear = function (Year) {
-        if (((Year % 4)==0) && ((Year % 100)!=0) || ((Year % 400)==0)) {
-        return (true);
+        if (( ((Year % 4)==0) && ((Year % 100)!=0) ) || ((Year % 400)==0)) {
+            return (true);
         } else { return (false); }
     }
 };
